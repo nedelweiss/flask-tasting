64d538be2b2f7766fc7e2436e68def9f5de465ff
nedelweiss <edelweissnull@gmail.com>
2025-05-16 02:28:11 +0300

    Added 2 examples of get & post requests with parameters & body

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, request
+
+from circle import Circle
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/area', methods=['GET'])
+def calculate_area():
+    radius = request.args.get("radius")
+    return area(radius)
+
+@app.route('/area2', methods=['POST'])
+def calculate_area2():
+    data = request.get_json(force=True)
+    radius = data['radius']
+    return area(radius)
+
+def area(radius):
+    circle = Circle(float(radius))
+    return str(circle.calculate_area())
 
 
 if __name__ == '__main__':
